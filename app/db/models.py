771c4b491d16d47b83eabafd9ddd953a708bc2a5
Vladislav <vlad.kharchenko.96@list.ru>
2024-07-25 17:32:33 +0500

    update models

--- app/db/models.py
+++ app/db/models.py
@@ -19,43 +19,54 @@ class User(Base):
     number: Mapped[str] = mapped_column(String(20), nullable=True)
     
 
+class Region(Base):
+    __tablename__ = 'regions'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=True)
+    
+
 class Category(Base):
     __tablename__ = "categories"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
+    name: Mapped[str] = mapped_column(String(25), nullable=True)
     
 #Дополнить    
 class Series(Base):
     __tablename__ = "series"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
+    name: Mapped[str] = mapped_column(String(25), nullable=True)
     
 # Дополнить
 class Additionally(Base):
     __tablename__ = "additionally"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
+    name: Mapped[str] = mapped_column(String(25), nullable=True)
  
     
 class Product(Base):
     __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120))
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
-    series_id: Mapped[int] = mapped_column(ForeignKey("series.id"))
-    additionally_id: Mapped[int] = mapped_column(ForeignKey("additionally.id"), nullable=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=True)
+    series_id: Mapped[int] = mapped_column(ForeignKey("series.id"), nullable=True)
     
     
 class Ticket(Base):
     __tablename__ = "tickets"
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user_number: Mapped[str] = mapped_column(ForeignKey("users.number"))
+    region: Mapped[str] = mapped_column(String(128), nullable=True)
+    category: Mapped[str] = mapped_column(String(25), nullable=True)
+    series: Mapped[str] = mapped_column(String(25), nullable=True)
+    product = Mapped[str] = mapped_column(String(256), nullable=True)
+    add = mapped_column(BigInteger)
+        
+    
 
 
     
